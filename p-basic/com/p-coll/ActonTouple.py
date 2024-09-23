7d13f3d267530806d0d362025614e17721d82c1e
MSrinivas1 <mangalipelli.srinivas@gmail.com>
2024-09-23 22:44:55 +0530

    tuple

--- p-basic/com/p-coll/ActonTouple.py
+++ p-basic/com/p-coll/ActonTouple.py
@@ -1,25 +1,34 @@
 
 
 def tupleAction() :
-    """ """
+    """ create simple tuple class """
     t = tuple([1,3,2,5,7])
     printTuple(t)
+    
 
 def tupleAction(t) :
-    """ """
+    """ convert collection tuple, list, set and dist to tuple """
     print(type(t))
     print(len(t))
     t = tuple(t)
-    printTuple(t)
+
+    #printTuple(t)
+    actionOntouple(t)
 
 def printTuple(t):
+    """ print touple class """
     for k in t:
         print(k)
-
-
+def actionOntouple(t):
+    """ tuple only support count and index"""
+    print("count value",t.count(36))
+    for i in range(0, len(t)):
+        print("from index ", format(t[i]))
+    
 if __name__ == "__main__":
     print(__name__)
-    
+    a = "12"
+    b = tuple(a)
     """ tuple can accept tuple """
     tupleAction(()) 
 
